3bc44b76a656ef9b802a12d2f0a9ca7eaa18305f
Jitendra <jitendraks1803@gmail.com>
2023-04-22 21:56:30 +0530

    fix for cron not updating price

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,6 +1,13 @@
 import intiliaseScrape from "@/app/apiHelpers/performScrape";
+import { scrapeAmazon } from "@/app/apiHelpers/scrapeAmazon";
+import { scrapeFlipkart } from "@/app/apiHelpers/scrapeFlipkart";
 import prisma from "@/app/utils/prisma";
-
+interface ItemDetails {
+  itemId: string;
+  itemProvider: string;
+  itemPrice: number;
+  itemUrl: string;
+}
 export async function GET() {
   try {
     const itemDetails = await prisma.watchlist.findMany({
@@ -11,7 +18,98 @@ export async function GET() {
         itemUrl: true,
       },
     });
-    intiliaseScrape(itemDetails);
+
+    itemDetails.forEach(async (itemDetail: ItemDetails) => {
+      console.log("called", itemDetail);
+      // async function performScrape(itemDetail: ItemDetails) {
+      if (itemDetail.itemProvider === "amazon") {
+        const productDetails = await scrapeAmazon(itemDetail.itemUrl);
+
+        // if the new price of productDetails is lower than the current price then
+        // update the itemPrice to new lower price
+        // update the itemLowestPrice to new lower price
+        // update the previous prices
+        if (productDetails.price < itemDetail.itemPrice) {
+          await prisma.watchlist.update({
+            where: { itemId: itemDetail.itemId },
+            data: {
+              itemPrice: productDetails.price,
+              itemPreviousPrice: itemDetail.itemPrice,
+              itemLowestPrice: productDetails.price,
+            },
+          });
+
+          await prisma.prices.create({
+            data: {
+              watchlistId: itemDetail.itemId,
+              price: productDetails.price,
+            },
+          });
+        } else if (productDetails.price > itemDetail.itemPrice) {
+          // if the price is higher then
+          // update current price
+          // update highest price
+
+          await prisma.watchlist.update({
+            where: { itemId: itemDetail.itemId },
+            data: {
+              itemPreviousPrice: itemDetail.itemPrice,
+              itemPrice: productDetails.price,
+              itemHighestPrice: productDetails.price,
+            },
+          });
+
+          await prisma.prices.create({
+            data: {
+              watchlistId: itemDetail.itemId,
+              price: productDetails.price,
+            },
+          });
+        }
+      } else if (itemDetail.itemProvider === "flipkart") {
+        const productDetails = await scrapeFlipkart(itemDetail.itemUrl);
+
+        if (productDetails.price < itemDetail.itemPrice) {
+          await prisma.watchlist.update({
+            where: { itemId: itemDetail.itemId },
+            data: {
+              itemPrice: productDetails.price,
+              itemPreviousPrice: itemDetail.itemPrice,
+              itemLowestPrice: productDetails.price,
+            },
+          });
+
+          await prisma.prices.create({
+            data: {
+              watchlistId: itemDetail.itemId,
+              price: productDetails.price,
+            },
+          });
+        } else if (productDetails.price > itemDetail.itemPrice) {
+          // if the price is higher then
+          // update current price
+          // update highest price
+
+          await prisma.watchlist.update({
+            where: { itemId: itemDetail.itemId },
+            data: {
+              itemPrice: productDetails.price,
+              itemPreviousPrice: itemDetail.itemPrice,
+              itemHighestPrice: productDetails.price,
+            },
+          });
+
+          await prisma.prices.create({
+            data: {
+              watchlistId: itemDetail.itemId,
+              price: productDetails.price,
+            },
+          });
+        }
+      }
+      // }
+    });
+
     return new Response(JSON.stringify({ message: "Cron success" }), {
       status: 200,
     });
@@ -21,3 +119,5 @@ export async function GET() {
     });
   }
 }
+
+// export const revalidate = "60"
